36441dfff4a36d1776f5f3e3ac99639839d40cce
Andrew Newport <anewport@outlook.com>
2025-06-17 08:07:43 -0400

    displayed test character as a basic card

--- js/app.js
+++ js/app.js
@@ -1,8 +1,20 @@
-import Alpine from 'alpinejs';
+import Alpine from 'https://unpkg.com/alpinejs?module';
+import { characterCard } from './components/characterCard.js';
 import { characterClasses } from './state/lookupTables.js';
+import { createCharacter } from './models/character.js';
+import { partyState } from './state/partyState.js';
+
+// default state
+const defaultCharacter = createCharacter("Character Name", "fighter");
+partyState.characters.push(defaultCharacter);
 
 window.Alpine = Alpine;
 
+// declaring global variables (required for AlpineJS)
+window.defaultCharacter = defaultCharacter;
+window.partyState = partyState
+window.characterCard = characterCard;
 window.characterClasses = characterClasses;
 
 Alpine.start();
+console.log('Alpine initialized');
