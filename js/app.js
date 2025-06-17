e714aab93b16800c1421cf3fccbbd3e57ffc6a39
Andrew Newport <anewport@outlook.com>
2025-06-16 20:47:42 -0400

    wiped everything but the models, state, and utilities while I start anew

--- js/app.js
+++ js/app.js
@@ -1,11 +1,5 @@
 import Alpine from 'alpinejs';
-import { statisticsComponent } from './components/statisticsComponent.js';
-// import { itemsComponent } from './components/itemsComponent.js';
-import { partyState } from "./state/partyState.js";
 
 window.Alpine = Alpine;
-
-window.statisticsComponent = statisticsComponent;
-// window.itemsComponent = itemsComponent;
 
 Alpine.start();
